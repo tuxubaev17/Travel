ab9b252c1ee425f7e3bdfee0f214e3a0989e2bcf
tuxubaev17 <tuxubaev17@mail.com>
2019-12-15 14:22:52 +0600

    add detail view

--- travel/apps/cities/urls.py
+++ travel/apps/cities/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import home
+from .views import home, CityDetailView
+
 
 app_name = 'cities'
 
 urlpatterns = [
+    path('detail/<int:pk>/', CityDetailView.as_view(), name='detail'),
     path('', home, name='home'),
 
 ]
--- travel/apps/cities/views.py
+++ travel/apps/cities/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 from .models import City
+from django.views.generic.detail import DetailView
+
 
 def home(request):
     city = City.objects.all()
     return render(request, 'cities/home.html', {'objects_list': city})
+
+
+class CityDetailView(DetailView):
+    model = City
+    queryset = model.objects.all()
+    context_object_name = 'object'
+    template_name = 'cities/detail.html'
