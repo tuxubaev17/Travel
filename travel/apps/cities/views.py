597d7f8c56cddf5bc99f9fa3b1ec4ced2413d6dd
tuxubaev17 <tuxubaev17@mail.com>
2019-12-17 16:21:02 +0600

    delete and update list od city, pagination and succes message

--- travel/apps/cities/views.py
+++ travel/apps/cities/views.py
@@ -1,19 +1,21 @@
 from django.shortcuts import render
 from .models import City
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .forms import CityForm
 from django.urls import reverse_lazy
+from django.core.paginator import Paginator
+from django.contrib.messages.views import SuccessMessageMixin
+from django.contrib import messages
+
 
 
 def home(request):
-    if request.method == 'POST':
-        form = CityForm(request.POST or None)
-        if form.is_valid():
-            print(form.cleaned_data)
-    form = CityForm()
     cities = City.objects.all()
-    return render(request, 'cities/home.html', {'objects_list': cities, 'form': form})
+    paginator = Paginator(cities, 2)
+    page = request.GET.get('page')
+    cities = paginator.get_page(page)
+    return render(request, 'cities/home.html', {'objects_list': cities})
 
 
 class CityDetailView(DetailView):
@@ -23,8 +25,27 @@ class CityDetailView(DetailView):
     template_name = 'cities/detail.html'
 
 
-class CityCreateView(CreateView):
+class CityCreateView(SuccessMessageMixin,CreateView):
     model = City
     form_class = CityForm
     template_name = 'cities/create.html'
     success_url = reverse_lazy('cities:home')
+    success_message = 'Город успешно создан!'
+
+
+class CityUpdateView(SuccessMessageMixin,UpdateView):
+    model = City
+    form_class = CityForm
+    template_name = 'cities/update.html'
+    success_url = reverse_lazy('cities:home')
+    success_message = 'Город успешно изменен!'
+
+
+class CityDeleteView(DeleteView):
+    model = City
+    template_name = 'cities/delete.html'
+    success_url = reverse_lazy('cities:home')
+
+    def get(self, request, *args, **kwargs):
+        messages.success(request, 'Город успешно удален!')
+        return self.post(request, *args, **kwargs)
